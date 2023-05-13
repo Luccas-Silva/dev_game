49a4ff37d7f0fe5e45a17206aee5bc05c0f87b4f
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2023-05-13 16:26:42 -0300

    Project created

---
+++ src/main/java/com/luccas/devgame/DevgameApplication.java
@@ -0,0 +1,13 @@
+package com.luccas.devgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luccas/devgame/DevgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccas.devgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
