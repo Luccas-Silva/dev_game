a4027109ab70a35a44b147ccc88176db8c45c422
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2023-05-14 07:51:42 -0300

    Consulta SQL, projection, busca de games por lista

---
+++ src/main/java/com/luccas/devgame/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.luccas.devgame.projections;
+
+public interface GameMinProjection {
+	
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+
+}
--- src/main/java/com/luccas/devgame/services/GameService.java
+++ src/main/java/com/luccas/devgame/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.luccas.devgame.dto.GameDTO;
 import com.luccas.devgame.dto.GameMinDTO;
 import com.luccas.devgame.entities.Game;
+import com.luccas.devgame.projections.GameMinProjection;
 import com.luccas.devgame.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList(); 
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList(); 
 	}
 
